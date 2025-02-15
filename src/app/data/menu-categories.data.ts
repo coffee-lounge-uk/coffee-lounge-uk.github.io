e128185b799f6ce435141dfdf4af1e01b3a98d32
Wazed Rifat <wazedrifat@gmail.com>
2025-02-15 23:04:05 +0600

    all category icons added

--- src/app/data/menu-categories.data.ts
+++ src/app/data/menu-categories.data.ts
@@ -2,16 +2,16 @@ import { MenuCategory } from '../enums/menu-category.enum';
 import { MenuCategoryItem } from './../interfaces/menu-category-item.interface';
 
 export const MenuCategoryItems: MenuCategoryItem[] = [
-  { Name: MenuCategory.BreakFast, IconPath: 'assets/icons/breakfast-icon.svg' },
-  { Name: MenuCategory.LunchSpecial, IconPath: 'assets/icons/lunch-special-icon.svg' },
-  { Name: MenuCategory.Salad, IconPath: 'assets/icons/salad-icon.svg' },
-  { Name: MenuCategory.Snacks, IconPath: 'assets/icons/snacks-icon.svg' },
-  { Name: MenuCategory.Desserts, IconPath: 'assets/icons/desserts-icon.svg' },
-  { Name: MenuCategory.Drinks, IconPath: 'assets/icons/drinks-icon.svg' },
-  { Name: MenuCategory.Smoothie, IconPath: 'assets/icons/smoothie-icon.svg' },
-  { Name: MenuCategory.Juice, IconPath: 'assets/icons/juice-icon.svg' },
-  { Name: MenuCategory.Coffe, IconPath: 'assets/icons/coffee-icon.svg' },
-  { Name: MenuCategory.Tea, IconPath: 'assets/icons/tea-icon.svg' },
-  { Name: MenuCategory.Sandwiches, IconPath: 'assets/icons/sandwiches-icon.svg' },
-  { Name: MenuCategory.BangladeshiStreetFood, IconPath: 'assets/icons/bangladeshi-street-food-icon.svg' }
+  { Name: MenuCategory.BreakFast, IconPath: 'assets/svg/breakfast.svg' },
+  { Name: MenuCategory.LunchSpecial, IconPath: 'assets/svg/lunch.svg' },
+  { Name: MenuCategory.Salad, IconPath: 'assets/svg/salad.svg' },
+  { Name: MenuCategory.Snacks, IconPath: 'assets/svg/snacks.svg' },
+  { Name: MenuCategory.Desserts, IconPath: 'assets/svg/dessert.svg' },
+  { Name: MenuCategory.Drinks, IconPath: 'assets/svg/drink.svg' },
+  { Name: MenuCategory.Smoothie, IconPath: 'assets/svg/smoothie.svg' },
+  { Name: MenuCategory.Juice, IconPath: 'assets/svg/juice.svg' },
+  { Name: MenuCategory.Coffe, IconPath: 'assets/svg/coffee.svg' },
+  { Name: MenuCategory.Tea, IconPath: 'assets/svg/tea.svg' },
+  { Name: MenuCategory.Sandwiches, IconPath: 'assets/svg/sandwich.svg' },
+  { Name: MenuCategory.BangladeshiStreetFood, IconPath: 'assets/svg/street-food.svg' },
 ];
