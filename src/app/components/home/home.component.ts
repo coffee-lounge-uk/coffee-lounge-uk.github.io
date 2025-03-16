b48a36bdd3771e7d9490d4dd87130c7b9586f98c
Wazed Rifat <wazedrifat@gmail.com>
2025-03-16 20:36:02 +0600

    updated theme
    # Conflicts:
    #	package-lock.json
    #	package.json

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,47 +8,22 @@ import { MenuCategory } from '../../enums/menu-category.enum';
 import { MenuComponent } from "../menu/menu.component";
 import { FooterComponent } from "../footer/footer.component";
 import { AboutComponent } from "../about/about.component";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, MenuComponent, AboutComponent, CurrencyPipe],
+  imports: [CommonModule, RouterModule, MenuComponent, AboutComponent, CurrencyPipe, NgbModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
 	specials: MenuItem[] = [];
-  menuCategories: MenuCategoryItem[] = [];
-  currentSpecialIndex: number = 0;
-  private intervalId: any;
 
   constructor(private router: Router, private menuService: MenuService) {}
 
   ngOnInit() {
     this.specials = this.menuService.getSpecialItems();
-    this.menuCategories = this.menuService.getCategories();
-    
-    this.startSlideshow();
-  }
-
-  ngOnDestroy() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
-
-  startSlideshow() {
-    this.intervalId = setInterval(() => {
-      this.nextSlide();
-    }, 5000);
-  }
-
-  prevSlide() {
-		this.currentSpecialIndex = (this.currentSpecialIndex + this.specials.length - 1) % this.specials.length;
-  }
-
-  nextSlide() {
-		this.currentSpecialIndex = (this.currentSpecialIndex + 1) % this.specials.length;
   }
 
 	goToItem(id: number) {
